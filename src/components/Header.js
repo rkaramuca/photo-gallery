68652908cab63b5db51ec84059682ef8bdd588a0
Ronald Karamuca <11005050@live.mercer.edu>
2022-05-05 00:43:54 -0400

    Added topbar and header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Your Photos</h1>
+      <p>Add inspirational quote api here...</p>
+    </div>
+  );
+}
+ 
+export default Header;
---
+++ src/components/Topbar.js
@@ -0,0 +1,9 @@
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <h1>PhotoGallery</h1>  
+    </div>
+  );
+}
+ 
+export default Topbar;
